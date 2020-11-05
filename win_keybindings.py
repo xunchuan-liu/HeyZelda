8a319c4d7a4964d494eb668b58c6ddb402bf2232
Aaron Keller <aaronkeller2021@u.northwestern.edu>
2020-11-05 17:31:49 -0600

    Added more functions, organized code

--- win_keybindings.py
+++ win_keybindings.py
@@ -24,7 +24,6 @@ class Keybinding:
         self.keyboard.release(Key.alt_l)
         self.keyboard.release('h')
         pyperclip.copy(message)
-        #pyperclip.paste()
         self.keyboard.press(Key.ctrl)
         self.keyboard.press('v')
         self.keyboard.release(Key.ctrl)
@@ -33,3 +32,41 @@ class Keybinding:
     def send_chat_message(self):
         self.keyboard.press(Key.enter)
         self.keyboard.release(Key.enter)
+        self.keyboard.press(Key.alt_l)
+        self.keyboard.press('h')
+        self.keyboard.release(Key.alt_l)
+        self.keyboard.release('h')
+
+    def quick_message(self, message):
+        self.type_chat_message(message)
+        self.send_chat_message()
+
+    def raise_lower_hand(self):
+        self.keyboard.press(Key.alt_l)
+        self.keyboard.press('y')
+        self.keyboard.release(Key.alt_l)
+        self.keyboard.release('y')
+    
+    def start_stop_local_recording(self):
+        self.keyboard.press(Key.alt_l)
+        self.keyboard.press('r')
+        self.keyboard.release(Key.alt_l)
+        self.keyboard.release('r')
+
+    def pause_resume_recording(self):
+        self.keyboard.press(Key.alt_l)
+        self.keyboard.press('p')
+        self.keyboard.release(Key.alt_l)
+        self.keyboard.release('p')
+
+    def share_stop_screen(self):
+        self.keyboard.press(Key.alt_l)
+        self.keyboard.press('s')
+        self.keyboard.release(Key.alt_l)
+        self.keyboard.release('s')
+
+    def read_speaker_name(self):
+        self.keyboard.press(Key.ctrl)
+        self.keyboard.press('2')
+        self.keyboard.release(Key.ctrl)
+        self.keyboard.release('2')
